aa9c8f1a1971db45df50c9cdf5b4aec7db102d7f
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-03-06 14:45:37 +0500

    Add files via upload

---
+++ Q7andQ8.js
@@ -0,0 +1,9 @@
+"use strict";
+// Addition
+console.log(6 + 2);
+// Subtraction
+console.log(16 - 8);
+// Multiplication
+console.log(2 * 4);
+// Division
+console.log(16 / 2);
---
+++ Q7andQ8.ts
@@ -0,0 +1,8 @@
+// Addition
+console.log(6+2)
+// Subtraction
+console.log(16 - 8)
+// Multiplication
+console.log(2 * 4)
+// Division
+console.log(16 / 2)
